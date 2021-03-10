ed8ef2975ab5ebbe7f625ac89a1031e22fd1dc7b
tpo-krobin13 <krobin13@travelers.com>
2021-03-10 13:58:25 -0500

    Update and Delete Entries

--- routes/articles.js
+++ routes/articles.js
@@ -33,7 +33,8 @@ router.post('/', asyncHandler(async (req, res) => {
 
 /* Edit article form. */
 router.get("/:id/edit", asyncHandler(async(req, res) => {
-  res.render("articles/edit", { article: {}, title: "Edit Article" });
+  const article = await Article.findByPk(req.params.id)
+  res.render("articles/edit", { article, title: "Edit Article" });
 }));
 
 /* GET individual article. */
@@ -44,16 +45,21 @@ router.get("/:id", asyncHandler(async (req, res) => {
 
 /* Update an article. */
 router.post('/:id/edit', asyncHandler(async (req, res) => {
-  res.redirect("/articles/");
+  const article = await Article.findByPk(req.params.id)
+  await article.update(req.body);
+  res.redirect("/articles/"+ article.id);
 }));
 
 /* Delete article form. */
 router.get("/:id/delete", asyncHandler(async (req, res) => {
-  res.render("articles/delete", { article: {}, title: "Delete Article" });
+  const article = await Article.findByPk(req.params.id)
+  res.render("articles/delete", { article, title: "Delete Article" });
 }));
 
 /* Delete individual article. */
 router.post('/:id/delete', asyncHandler(async (req ,res) => {
+  const article = await Article.findByPk(req.params.id)
+  article.destroy();
   res.redirect("/articles");
 }));
 
